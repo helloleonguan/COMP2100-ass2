006bd1bb98f39046b17c07434d801fbf7968e272
u5684206 <u5684206@anu.edu.au>
2016-04-13 00:35:08 +1000

    Add events associated with buttons

--- app/src/main/java/assignments/comp2100/calculator/MainActivity.java
+++ app/src/main/java/assignments/comp2100/calculator/MainActivity.java
@@ -2,15 +2,99 @@ package assignments.comp2100.calculator;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    TextView tvDisplay;
+
+    Button btAC;
+    Button btLeftBracket;
+    Button btRightBracket;
+    Button btMultiplication;
+    Button btSeven;
+    Button btEight;
+    Button btNine;
+    Button btDivision;
+    Button btFour;
+    Button btFive;
+    Button btSix;
+    Button btAddition;
+    Button btOne;
+    Button btTwo;
+    Button btThree;
+    Button btSubtraction;
+    Button btUndo;
+    Button btZero;
+    Button btDot;
+    Button btEqual;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tvDisplay = (TextView) findViewById(R.id.displayArea);
+        tvDisplay.setMovementMethod(new ScrollingMovementMethod());
+        tvDisplay.setText("");
+
+        btAC = (Button) findViewById(R.id.actAllClear);
+        btLeftBracket = (Button) findViewById(R.id.inLeftBracket);
+        btRightBracket = (Button) findViewById(R.id.inRightBracket);
+        btMultiplication = (Button) findViewById(R.id.inMultiplication);
+        btSeven = (Button) findViewById(R.id.inSeven);
+        btEight = (Button) findViewById(R.id.inEight);
+        btNine = (Button) findViewById(R.id.inNine);
+        btDivision = (Button) findViewById(R.id.inDivision);
+        btFour = (Button) findViewById(R.id.inFour);
+        btFive = (Button) findViewById(R.id.inFive);
+        btSix = (Button) findViewById(R.id.inSix);
+        btAddition = (Button) findViewById(R.id.inAddition);
+        btOne = (Button) findViewById(R.id.inOne);
+        btTwo = (Button) findViewById(R.id.inTwo);
+        btThree = (Button) findViewById(R.id.inThree);
+        btSubtraction = (Button) findViewById(R.id.inSubtraction);
+        btUndo = (Button) findViewById(R.id.actUndo);
+        btZero = (Button) findViewById(R.id.inZero);
+        btDot = (Button) findViewById(R.id.inDot);
+        btEqual = (Button) findViewById(R.id.actEqual);
+
     }
 
+
+    public void append(View v) {
+        int pre_line_count = tvDisplay.getLineCount();
+        tvDisplay.setText(tvDisplay.getText().toString() + ((Button) v).getText().toString());
+        if (pre_line_count >= 4 && pre_line_count - tvDisplay.getLineCount() == -1) {
+            tvDisplay.scrollTo(0, tvDisplay.getScrollY() + tvDisplay.getLineHeight());
+        }
+    }
+
+    public void allClear(View v) {
+        tvDisplay.setText("");
+    }
+
+    public void undo(View v) {
+        if (!tvDisplay.getText().toString().equals("")) {
+            String tmp = tvDisplay.getText().toString();
+            tvDisplay.setText(tmp.substring(0, tmp.length() - 1));
+        }
+    }
+
+    public void evaluate(View v) {
+        String expression = tvDisplay.getText().toString();
+
+        // TODO check whether the expression is legit
+        if (true) {
+            // TODO pass the expression to the parse algorithm and get the result
+            tvDisplay.setText("Not available now.");
+        } else {
+            // TODO display error message
+        }
+    }
 
 }
