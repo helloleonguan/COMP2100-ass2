cae21f682776696e9afff40f0bd9cdfaa87bd5e5
u5684206 <u5684206@anu.edu.au>
2016-04-13 14:29:47 +1000

    Improve scrolling textview & add comments & add timer to error msg

--- app/src/main/java/assignments/comp2100/calculator/MainActivity.java
+++ app/src/main/java/assignments/comp2100/calculator/MainActivity.java
@@ -3,13 +3,19 @@ package assignments.comp2100.calculator;
 import android.app.Activity;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+//        Author: Liyang(Leon) Guan
+//        Uni ID: u5684206
+//        Declaration: The following code is written all by myself.
+
 public class MainActivity extends Activity {
 
+    // View declarations
     TextView tvDisplay;
 
     Button btAC;
@@ -39,10 +45,12 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // setup the textView
         tvDisplay = (TextView) findViewById(R.id.displayArea);
         tvDisplay.setMovementMethod(new ScrollingMovementMethod());
         tvDisplay.setText("");
 
+        // linking all buttons
         btAC = (Button) findViewById(R.id.actAllClear);
         btLeftBracket = (Button) findViewById(R.id.inLeftBracket);
         btRightBracket = (Button) findViewById(R.id.inRightBracket);
@@ -66,7 +74,10 @@ public class MainActivity extends Activity {
 
     }
 
-
+    /**
+     * This method append the symbol of an input button to the String in the textView and scroll the textView to an appropriate position.
+     * @param v
+     */
     public void append(View v) {
         int max_lines;
         if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
@@ -80,28 +91,65 @@ public class MainActivity extends Activity {
         if (pre_line_count >= max_lines) {
             tvDisplay.scrollTo(0, tvDisplay.getLineHeight() * (tvDisplay.getLineCount() - max_lines));
         }
-
-
     }
 
+    /**
+     * This method cleans the textView and scroll it to the top.
+     * @param v
+     */
     public void allClear(View v) {
         tvDisplay.setText("");
+        tvDisplay.scrollTo(0,0);
     }
 
+    /**
+     * This method deletes the last input character from the textView and  scroll the textView to an appropriate position.
+     * @param v
+     */
     public void undo(View v) {
         if (!tvDisplay.getText().toString().equals("")) {
             String tmp = tvDisplay.getText().toString();
             tvDisplay.setText(tmp.substring(0, tmp.length() - 1));
         }
+
+        int max_lines;
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            max_lines = 4;
+        } else {
+            max_lines = 2;
+        }
+        if (tvDisplay.getLineCount() > max_lines) {
+            tvDisplay.scrollTo(0, tvDisplay.getLineHeight() * (tvDisplay.getLineCount() - max_lines));
+        } else {
+            tvDisplay.scrollTo(0,0);
+        }
     }
 
+    /**
+     * This method first checks whether the expression in the textView is legit and then evaluates the expression
+     * using a tree parsing algorithm. It returns the result to the textView if the expression is legit. Otherwise,
+     * returns an error message.
+     * @param v
+     */
     public void evaluate(View v) {
         String expression = tvDisplay.getText().toString();
 
         // TODO check whether the expression is legit
         if (true) {
             // TODO pass the expression to the parse algorithm and get the result
-            tvDisplay.setText("Not available now.");
+            tvDisplay.scrollTo(0,0);
+            new CountDownTimer(5000, 500) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    tvDisplay.setText("Not Available now! \n Disappear in " + millisUntilFinished / 1000 + "s.");
+                }
+
+                @Override
+                public void onFinish() {
+                    allClear(tvDisplay);
+                }
+            }.start();
+
         } else {
             // TODO display error message
         }
